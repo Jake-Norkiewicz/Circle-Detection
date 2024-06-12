d37140c4a6a1159d4968e83bada4a799df240ef2
HubertCzuprynski <148956568+HubertCzuprynski@users.noreply.github.com>
2024-06-12 11:31:34 +0200

    Update control.py Hubert

--- control.py
+++ control.py
@@ -3,14 +3,16 @@ from ultralytics import YOLO
 from threading import Thread
 import time
 
+
 def detect_and_show(drone, model) -> None:
     while True:
         drone.detect_circles(model)
-        drone.show_camera(delay = 1)
+        drone.show_camera(delay=1)
+
 
 def main() -> None:
     drone = TelloDrone()
-    model = YOLO("circles600last.pt", verbose = False)
+    model = YOLO("circles600last.pt", verbose=False)
     half_width = 480
     tolerance = 20
 
@@ -19,15 +21,15 @@ def main() -> None:
 
     drone.Takeoff()
 
-    camera_thread = Thread(target = detect_and_show, args = [drone, model])
+    camera_thread = Thread(target=detect_and_show, args=[drone, model])
     print("Starting threads")
     camera_thread.start()
 
     time.sleep(5)
 
     command_sent = False
-    #first_rotate = False
-    #start_time = time.time()
+    # first_rotate = False
+    # start_time = time.time()
 
     drone.fly(0, 0, 0, 0)
 
@@ -54,41 +56,28 @@ def main() -> None:
             # in tolerance field
             else:
                 drone.fly(0, 0, 0, 0)
-                drone.fly(0, 0, 0, 0)
-                drone.fly(0, 0, 0, 0)
                 print("stopping")
                 break
-        
+
         else:
             drone.fly(0, 0, 0, 0)
 
-
     print("out of loop")
-    time.sleep(2)
-
-    command_sent = False
-    # drone.fly(25, 0, 0, 0)
-    # time.sleep(8)
 
     while True:
-        if drone.circle_count >= 2 and not command_sent:
-            drone.fly(25, 0, 0, 0)
-            command_sent = True
+        if drone.circle_count >= 2:
+            drone.fly(15, 0, 0, 0)
 
-        elif not drone.circle_count >= 2 and not command_sent:
-            drone.fly(25, 0 ,0, 0)
-            command_sent = True
+        else:
             time.sleep(3)
             break
 
     drone.fly(0, 0, 0, 0)
-    time.sleep(1)
     drone.Land()
 
     camera_thread.join()
 
     del drone
-
 
 if __name__ == "__main__":
     main()
